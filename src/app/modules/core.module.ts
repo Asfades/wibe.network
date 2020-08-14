fbec22b748d116b6527c118639b7c32b3ffd19ac
Serhii Horbenko <serhiihorbenko@Serhiis-MacBook-Pro.local>
2020-08-15 00:43:05 +0300

    Add Modal component and Material module into Core

--- src/app/modules/core.module.ts
+++ src/app/modules/core.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
+import { ModalComponent } from '../components/modal/modal.component';
+import { MaterialModule } from './material.module';
+
+const declarations = [
+  ModalComponent
+];
 
 @NgModule({
-  providers: [
-    
-  ]
+  imports: [
+    MaterialModule
+  ],
+  declarations,
+  providers: [],
+  exports: declarations
 })
 export class CoreModule {}
