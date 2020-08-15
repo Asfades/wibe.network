3c927100263f8decfcb3171fe4d6ecda8550b15d
Serhii Horbenko <serhiihorbenko@Serhiis-MacBook-Pro.local>
2020-08-16 01:05:07 +0300

    Add Core and ImageCropper modules

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
-import { ProfileComponent } from './profile.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+import { ImageCropperModule } from 'ngx-image-cropper';
+
 import { ActivateProfileGuard } from './activateProfile.guard';
+import { ProfileComponent } from './profile.component';
 import { TracksComponent } from './tracks/tracks.component';
+import { CoreModule } from '@src/app/modules/core.module';
 
 const declarations = [
   ProfileComponent,
-  TracksComponent
+  TracksComponent,
 ];
 
 @NgModule({
   declarations,
   imports: [
+    CoreModule,
+    ImageCropperModule,
     CommonModule,
     RouterModule.forChild([
       {
