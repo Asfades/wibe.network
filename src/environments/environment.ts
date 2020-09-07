ac7f6efa16dfe9e112f4553683b31175d4927f81
Serhii Horbenko <serhiihorbenko@Serhiis-MacBook-Pro.local>
2020-09-07 21:22:59 +0300

    Remove firebase dev env and add auth api urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,10 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyCM3crwG9zRe9o8c0lpuiQN2aBqHkdnpss',
-    authDomain: 'wibe-network.firebaseapp.com',
-    databaseURL: 'https://wibe-network.firebaseio.com',
-    projectId: 'wibe-network',
-    storageBucket: 'wibe-network.appspot.com',
-    messagingSenderId: '247394393361',
-    appId: '1:247394393361:web:a276baf526ce47b08ac0b0',
-    measurementId: 'G-JTBQZ53WK6'
+  api: {
+    signUp: 'http://localhost:3000/auth/signup',
+    signIn: 'http://localhost:3000/auth/signin',
+    refreshAccessToken: 'http://localhost:3000/auth/refresh-access-token',
   }
 };
 
