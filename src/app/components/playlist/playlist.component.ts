f17415fb7f9587d3760645343046f1434e7ad6e7
Serhii Horbenko <serhiihorbenko@Serhiis-MacBook-Pro.local>
2020-09-09 18:47:03 +0300

    Remove unused staf

--- src/app/components/playlist/playlist.component.ts
+++ src/app/components/playlist/playlist.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subscription, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import * as playlistActions from './store/playlist.actions';
 import { Track } from '@entities/track.model';
@@ -11,9 +11,8 @@ import * as fromApp from '../../store/app.reducer';
   templateUrl: './playlist.component.html',
   styleUrls: ['./playlist.component.scss']
 })
-export class PlaylistComponent implements OnInit, OnDestroy {
-  playlist: Observable<{ tracks: Track[], trackId: number }>;
-  sub: Subscription;
+export class PlaylistComponent implements OnInit {
+  playlist$: Observable<{ tracks: Track[], trackId: number }>;
 
   constructor(
     private store: Store<fromApp.AppState>
@@ -21,7 +20,7 @@ export class PlaylistComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.store.dispatch(new playlistActions.FetchPlaylist());
-    this.playlist = this.store.select('playlist');
+    this.playlist$ = this.store.select('playlist');
   }
 
   chooseTrack(index: number) {
@@ -30,8 +29,5 @@ export class PlaylistComponent implements OnInit, OnDestroy {
 
   trackByIndex(index: number) {
     return index;
-  }
-
-  ngOnDestroy() {
   }
 }
