ca159eb8682227afc85e579f7179e8b0d357de7c
Serhii Horbenko <serhii@we-tribu.com>
2021-04-07 20:35:52 +0300

    Fix build env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  api: {
+    signUp: 'http://localhost:3000/auth/signup',
+    signIn: 'http://localhost:3000/auth/signin',
+    refreshAccessToken: 'http://localhost:3000/auth/refresh-access-token',
+    audio: {
+      files: 'http://localhost:3000/audio/files'
+    }
+  }
 };
